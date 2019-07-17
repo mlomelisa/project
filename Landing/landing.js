a781d075f71947bea33086f1683b5ab29ff686d9
mlomelisa <monica.lomelim@gmail.com>
2019-07-17 16:07:57 -0700

    Fixed modal issue after merge

--- Landing/landing.js
+++ Landing/landing.js
@@ -97,61 +97,61 @@ $('#googleLogin').on('click', function(e){
 
   $('#loginModal').modal('hide');
   // $('#myModalDiet').modal('show');
- console.log('Im here')
+ 
 });
 
 $('#anonymousLogin').click(function(e){
   e.preventDefault();
   $('#loginModal').modal('hide');
   // $('#myModalDiet').modal('show');
-  console.log('Im here')
+ 
  });
 
  /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-// function onEatHealthy_function() {
-// document.getElementById("myCarousel").style.visibility ="collapse";
-// document.getElementById("eatRightDiv1").style.visibility ="visible";
-//  document.getElementById("eatRightDiv1").style.height ="80%";
-//  document.getElementById("eatRightDiv1").setAttribute("src","https://www.youtube.com/embed/28CIFOhkKrU")
+function onEatHealthy_function() {
+document.getElementById("myCarousel").style.visibility ="collapse";
+document.getElementById("eatRightDiv1").style.visibility ="visible";
+ document.getElementById("eatRightDiv1").style.height ="80%";
+ document.getElementById("eatRightDiv1").setAttribute("src","https://www.youtube.com/embed/28CIFOhkKrU")
  
  
-// }
-// function onActive_function(){
-//     document.getElementById("myCarousel").style.visibility ="collapse";
-// document.getElementById("eatRightDiv1").style.visibility ="visible";
-//  document.getElementById("eatRightDiv1").style.height ="80%";
-//  document.getElementById("eatRightDiv1").setAttribute("src","Images/active.jpg")
+}
+function onActive_function(){
+    document.getElementById("myCarousel").style.visibility ="collapse";
+document.getElementById("eatRightDiv1").style.visibility ="visible";
+ document.getElementById("eatRightDiv1").style.height ="80%";
+ document.getElementById("eatRightDiv1").setAttribute("src","Images/active.jpg")
  
-// }
+}
 
-// function onHomeClick(){
-//     document.getElementById("myCarousel").style.visibility ="visible";
-//     document.getElementById("eatRightDiv1").style.visibility ="collapse";
-//     document.getElementById("eatRightDiv1").style.height ="0%";
-//     document.getElementById("eatRightDiv1").setAttribute("src",null);
-// }
+function onHomeClick(){
+    document.getElementById("myCarousel").style.visibility ="visible";
+    document.getElementById("eatRightDiv1").style.visibility ="collapse";
+    document.getElementById("eatRightDiv1").style.height ="0%";
+    document.getElementById("eatRightDiv1").setAttribute("src",null);
+}
 
-// function filterFunction() {
-//  var input, filter, ul, li, a, i;
-//    input = document.getElementById("myInput");
-//   filter = input.value.toUpperCase();
-//   div = document.getElementById("myDropdown");
-//   a = div.getElementsByTagName("a");
-//   for (i = 0; i < a.length; i++) {
-//     txtValue = a[i].textContent || a[i].innerText;
-//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//       a[i].style.display = "";
-//     } else {
-//       a[i].style.display = "none";
-//     }
+function filterFunction() {
+ var input, filter, ul, li, a, i;
+   input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
 
-//   }
-// }
+  }
+}
  
 
 
